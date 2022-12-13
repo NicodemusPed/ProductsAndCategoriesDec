d1342e3cb1b004ff7bc0ea7fe6026ffc7a1018b5
NicodemusPed <107951768+NicodemusPed@users.noreply.github.com>
2022-12-12 16:21:17 -0800

    asd

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@ using ProductsAndCategoriesDec.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddSession();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<MyContext>(options =>
 {
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
